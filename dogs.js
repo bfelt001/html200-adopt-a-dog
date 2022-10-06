d14dfd2c5f1a89018a435d5b505cea5d1ed850b0
Breanne Felton <breanne.felton@gmail.com>
2022-10-05 19:11:05 -0700

    l4-a2

---
+++ dogs.js
@@ -0,0 +1,16 @@
+function clickDog(dogname,breed,fees){
+    
+    let dogs = ('Name: ' + dogname + ' Breed: ' +breed +  ' Fee: ' + fees);
+    
+alert(dogs);
+}
+
+
+var total = 0;
+function addFee(fee) {
+    var a = Number(fee);
+    total += a;
+    document.getElementById("totalFee").innerHTML = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(total);
+    alert(new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(total));
+}
+  
